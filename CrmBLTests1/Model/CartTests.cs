234320053335f7d319874e7afa0b15826beaf3a8
Артем <Tyrant997@gmail.com>
2022-06-01 15:56:00 +0700

    Добавлены Unit-тесты для классов Product и Cart

---
+++ CrmBLTests1/Model/CartTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CartTest()
+        {
+            //arrange - объявление входных и итоговых данных
+            var customer = new Customer() { Name = "testuser" };
+            var product1 = new Product() { Name = "pr1", Price = 100, Count = 10 };
+            var product2 = new Product() { Name = "prod2", Price = 200, Count = 20 };
+            var cart = new Cart(customer);
+            
+            var expectedResult = new List<Product> { product1, product1, product2 };
+
+            //act - непосредственно выполнение действий
+            cart.Add(product1);
+            cart.Add(product1);
+            cart.Add(product2);
+
+            var cartResult = cart.GetAll();
+            //assert - сравнение того что ожидаем и того что получили по факту
+            Assert.AreEqual(expectedResult.Count, cart.GetAll().Count);
+            for(int i = 0; i < expectedResult.Count;i++)
+            {
+                Assert.AreEqual(expectedResult[i], cartResult[i]);
+            }
+            //Assert.Fail();
+        }
+
+        /*
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            Assert.Fail();
+        }*/
+    }
+}
---
+++ CrmBLTests1/Model/ProductTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void ProductTest()
+        {
+            //arrange
+            //Начальные данные
+            Product product = new Product() { ProductID = 1, Name = "Камера для велосипеда"};
+            bool AreProductEqual = false;
+            //Ожидаемые
+            Product result_product = new Product() { ProductID = 1, Name = "Камера для велосипеда" };
+            
+            //act
+            Product result_product_created = new Product() { ProductID = product.GetHashCode(), Name = product.ToString() };
+            if (result_product == result_product_created) AreProductEqual = true;
+
+            //assert
+            //Assert.AreEqual(AreProductEqual, true);
+            Assert.AreEqual(result_product_created, result_product);
+            //Assert.Fail();
+        }
+
+        /*
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+        */
+    }
+}
