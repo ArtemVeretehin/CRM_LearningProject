971fa3e45be846810f2389d704ed1fdb52134eb9
Артем <Tyrant997@gmail.com>
2022-06-04 22:32:54 +0700

    Добавлен многопоточный вариант компьютерной модели (пока все кривовато, надо поразбираться)

---
+++ CrmBL/Model/ShopComputerModel_ForThreads.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    public class ShopComputerModel_ForThreads
+    {
+        Generator Generator = new Generator();
+        bool isWorking = false;      
+
+        public List<CashDesk> CashDesks { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<Check> Checks { get; set; }
+        public List<Sell> Sell { get; set; }
+        public Queue<Seller> Sellers { get; set; }
+
+        public ShopComputerModel_ForThreads()
+        {
+            CashDesks = new List<CashDesk>();
+            Carts = new List<Cart>();
+            Checks = new List<Check>();
+            Sell = new List<Sell>();
+            Sellers = new Queue<Seller>();
+
+            //Генерируем 20 продавцов (Seller) и ставим их в очередь Sellers
+            var sellers = Generator.GetNewSellers(20);
+            foreach (var seller in sellers)
+            {
+                Sellers.Enqueue(seller);
+            }
+
+            //Генерируем 20 продуктов
+            Generator.GetNewProducts(1000);
+
+            //В список касс добавляются три новых кассы, продавцами на которых являются три первых продавца в сгенерированной очереди рандомных продавцов
+            for (int i = 0; i < 3; i++)
+            {
+                CashDesks.Add(new CashDesk(CashDesks.Count, Sellers.Dequeue()));
+            }
+        }
+
+        public void Start()
+        {
+            isWorking = true;
+
+            //Создание задачи для генерации корзин и распределения корзин по кассам
+            Task.Run(() => CreateCarts(10, 1000));
+            
+            //Создание задач на обработку покупок в кассах
+            var cashDeskTasks = CashDesks.Select(c => new Task(() => CashDeskWork(c, 1000)));
+
+            //Запуск задач
+            foreach (var task in cashDeskTasks) task.Start();
+        }
+
+        //Функция генерации корзин (создание клиента, создание на основе него корзины, добавление продуктов в корзину) и распределение созданных корзин по кассам.
+        private void CreateCarts(int customersCount, int sleep)
+        {
+            while (isWorking)
+            {
+                //Создание 10 рандомных клиентов
+                var customers = Generator.GetNewCustomers(customersCount);
+
+                //Для каждого рандомного клиента создается корзина, которая забивается случайным количеством случайных продуктов. Созданная корзина ставится на кассу с наименьшей загруженностью
+                foreach (var customer in customers)
+                {      
+                    var cart = new Cart(customer);
+
+                    foreach (var prod in Generator.GetRandomProducts(10, 30))
+                    {
+                        cart.Add(prod);
+                    }
+
+                    //Выбор кассы с наименьшим количеством корзин в очереди
+                    var cash = CashDesks.FirstOrDefault(x => x.Count == CashDesks.Min(c => c.Count));
+
+                    //Постановка корзины в выбранную кассу
+                    cash.Enqueue(cart);
+                }
+
+                Thread.Sleep(sleep);
+            }
+        }
+
+        private void CashDeskWork(CashDesk cashDesk, int sleep)
+        {
+            while (isWorking)
+            {
+                if (cashDesk.Count > 0)
+                {
+                    cashDesk.Dequeue();
+                    Thread.Sleep(sleep);
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            isWorking = false;
+        }
+    }
+}
---
+++ CrmBLTests1/Model/ShopComputerModel_ForThreadsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class ShopComputerModel_ForThreadsTests
+    {
+        
+
+        [TestMethod()]
+        public void StartTest()
+        {
+            var model = new ShopComputerModel_ForThreads();
+            model.Start();
+            Thread.Sleep(10000);
+        }
+
+        
+    }
+}
