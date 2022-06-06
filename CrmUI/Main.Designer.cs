0d24bfa0819d24de25a13edc27d5f106f6980c70
Артем <Tyrant997@gmail.com>
2022-06-06 22:15:42 +0700

    Доработан UI: 1. Возможность авторизации 2. Возможность заполнения своей корзины 3. Возможность формирования и отправки данных по покупке в БД.

--- CrmUI/Main.Designer.cs
+++ CrmUI/Main.Designer.cs
@@ -38,7 +38,12 @@
             this.customerAddToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.checkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -127,25 +132,80 @@
             this.ModelToolStripMenuItem.Text = "Моделирование";
             this.ModelToolStripMenuItem.Click += new System.EventHandler(this.ModelToolStripMenuItem_Click);
             // 
-            // label1
+            // listBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(370, 152);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(264, 404);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(333, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(215, 340);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Итого";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(499, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Оплатить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(666, 32);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(130, 16);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Здравствуй, гость.";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -164,8 +224,13 @@
         private System.Windows.Forms.ToolStripMenuItem productAddToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sellerAddToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem customerAddToolStripMenuItem2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ToolStripMenuItem ModelToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- CrmUI/Main.cs
+++ CrmUI/Main.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CrmBL.Model;
@@ -14,10 +15,19 @@ namespace CrmUI
     public partial class Main : Form
     {
         CrmContext db;
+        Cart cart;
+        Customer customer;
+        CashDesk cashDesk;
+
         public Main()
         {
             InitializeComponent();
             db = new CrmContext();
+            cart = new Cart(customer);
+            cashDesk = new CashDesk(1, db.Sellers.FirstOrDefault())
+            {
+                IsModel = false
+            };
         }
 
         private void productToolStripMenuItem_Click(object sender, EventArgs e)
@@ -80,6 +90,71 @@ namespace CrmUI
         {
             var ModelForm = new ModelForm();
             ModelForm.Show();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Task Products_Load = Task.Run(() =>
+            {
+                var products = db.Products.ToArray();
+                listBox1.Invoke((Action)delegate { listBox1.Items.AddRange(products); });
+            });
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Product product)
+            { 
+                listBox2.Items.Add(product);
+                cart.Add(product);
+                label3.Text = System.Convert.ToString(cart.Price);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login login = new Login();
+            if (login.ShowDialog() == DialogResult.OK)
+            {
+                //if (db.Customers.Where(c => c.Name == login.Customer.Name).Count() > 0)
+                
+                var tempCustomer = db.Customers.FirstOrDefault(c => c.Name.Equals(login.Customer.Name));
+                if (tempCustomer != null)
+                { 
+                    customer = tempCustomer;
+                    cart.Customer = customer;
+                    linkLabel1.Text = $"Здравствуй, {tempCustomer.Name}";
+                }
+                else
+                {
+                    //db.Customers.Add(login.Customer);
+                    //db.SaveChanges();
+                    //customer = login.Customer;
+                    LoginFailed loginfailedform = new LoginFailed();
+                    if (loginfailedform.ShowDialog() == DialogResult.OK)
+                    {
+                        loginfailedform.Close();
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.Dispose();
+            if(customer !=null)
+            {
+                cashDesk.Enqueue(cart);
+                cashDesk.Dequeue();
+                listBox2.Items.Clear();
+                cart = new Cart(customer);
+
+                MessageBox.Show("Покупка выполнена успешно","Успех",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Требуется авторизация!");
+            }
         }
     }
 }
